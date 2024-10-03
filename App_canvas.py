60d577287f0361b8c271e8a66b8be65957b153eb
mariii02 <mechever18@eafit.edu.co>
2024-10-03 09:57:35 -0500

    Update App_canvas.py

--- App_canvas.py
+++ App_canvas.py
@@ -8,12 +8,23 @@ st.subheader("Utiliza el tablero para hacer arte")
 # Add canvas component
 # Specify canvas parameters in application
 drawing_mode = "freedraw"
-stroke_width = st.slider('Selecciona el ancho de línea', 1, 30, 15)
 stroke_color = '#FFFFFF' # Set background color to white
 bg_color = '#000000'
 
 # Add side bar
-add_selectbox = st.sidebar.selectbox(
-    "Propiedades del tablero",
-    ("freedraw", "line", "circle")
+with st.sidebar
+    stroke_width = st.slider('Selecciona el ancho de línea', 1, 30, 15)
+   
+# Create a canvas component
+canvas_result = st_canvas(
+    fill_color="rgba(255, 165, 0, 0.3)",  # Fixed fill color with some opacity
+    stroke_width=stroke_width,
+    stroke_color=stroke_color,
+    background_color=bg_color,
+    height=500,
+    width=500,
+    key="canvas",
 )
+
+
+   
